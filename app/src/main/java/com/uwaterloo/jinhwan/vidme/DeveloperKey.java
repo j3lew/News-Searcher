d572baf8aa99076c92758be267af42616a43c039
Charlie Lew <37677245+j3lew@users.noreply.github.com>
2019-05-27 21:19:46 -0400

    Update DeveloperKey.java

--- app/src/main/java/com/uwaterloo/jinhwan/vidme/DeveloperKey.java
+++ app/src/main/java/com/uwaterloo/jinhwan/vidme/DeveloperKey.java
@@ -1,5 +1,5 @@
 package com.uwaterloo.jinhwan.vidme;
 
 public class DeveloperKey {
-    public static final String DEVELOPER_KEY = "AIzaSyCb3cD7azej7gGNOqDElXXzBXm9xrMJ0Yc";
+    public static final String DEVELOPER_KEY = "---------------------------";
 }
